ddc626a798c0bf0cdc10bafc1b2253cf294016c2
Faber Hoyos <faberhoyos01@gmail.com>
2022-04-30 14:25:14 -0500

    Mini estructura

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pageOne.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(
-        child: Text("Hola mundo!")
-      ),
+      home: PageOne()
     );
   }
 }
---
+++ lib/pageTwo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PageTwo extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Challenger'),
+      ),
+      body: Center(
+        child: Text('Page Two'),
+      ),
+    );
+  }
+}
